0be43dc9fa8f8692f4f0d3eb7a79bbe793a15654
tech47 <aayushineeshu@gmail.com>
2019-05-13 17:53:47 +0530

    First-commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import styled from 'styled-components';
 
-export default () => <div>Hello world!</div>
+
+
+export default () => (
+    <h1 style={{color: '#6b5b95'}}>
+    Hello world!
+    </h1>
+)
+    
+    
